070773b5f4236bf8aacaabf8f12bc97b751c1a8a
Arief Adi Nugroho <arief.ilkomp@gmail.com>
2025-01-09 11:37:42 +0700

    Add meta description pada halaman isi form

--- resources/views/form/must-login.blade.php
+++ resources/views/form/must-login.blade.php
@@ -1,9 +1,12 @@
 <x-common-layout>
-    <x-nav-header></x-nav-header>
+    @push('head')
+        <meta name="description" content="{{ $form?->description }}" itemprop="description" />
+        <meta name="title" content="{{ $form?->name }}" />
+        <meta name="originalTitle" content="{{ $form?->name }}" />
+    @endpush
     <div class="p-4 my-20 max-w-xl m-auto">
         <div class="p-4 text-2xl font-bold border border-gray-200 sm:p-8 bg-white dark:bg-gray-800 shadow-lg sm:rounded-lg">
             Anda Harus <a href="{{ route('login') }}" class="text-blue-500">Login</a> terlebih dahulu untuk mengisi form ini.
         </div>
-
     </div>
 </x-common-layout>
